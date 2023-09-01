697e9af5df44847abb3b82a0779d836711adc3fa
Sotunde Emmanuel <sotundeemmanuel@gmail.com>
2023-09-01 15:02:06 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,22 +18,64 @@ console.log(dataMon, barMon);
 
 barMon.addEventListener("click", function () {
   dataMon.style.visibility = "visible";
+  dataTue.style.visibility = "hidden";
+  dataWed.style.visibility = "hidden";
+  dataThur.style.visibility = "hidden";
+  dataFri.style.visibility = "hidden";
+  dataSat.style.visibility = "hidden";
+  dataSun.style.visibility = "hidden";
 });
 barTue.addEventListener("click", function () {
   dataTue.style.visibility = "visible";
+  dataMon.style.visibility = "hidden";
+  dataWed.style.visibility = "hidden";
+  dataThur.style.visibility = "hidden";
+  dataFri.style.visibility = "hidden";
+  dataSat.style.visibility = "hidden";
+  dataSun.style.visibility = "hidden";
 });
 barWed.addEventListener("click", function () {
   dataWed.style.visibility = "visible";
+  dataMon.style.visibility = "hidden";
+  dataTue.style.visibility = "hidden";
+  dataThur.style.visibility = "hidden";
+  dataFri.style.visibility = "hidden";
+  dataSat.style.visibility = "hidden";
+  dataSun.style.visibility = "hidden";
 });
 barThur.addEventListener("click", function () {
   dataThur.style.visibility = "visible";
+  dataMon.style.visibility = "hidden";
+  dataTue.style.visibility = "hidden";
+  dataWed.style.visibility = "hidden";
+  dataFri.style.visibility = "hidden";
+  dataSat.style.visibility = "hidden";
+  dataSun.style.visibility = "hidden";
 });
 barFri.addEventListener("click", function () {
   dataFri.style.visibility = "visible";
+  dataMon.style.visibility = "hidden";
+  dataTue.style.visibility = "hidden";
+  dataWed.style.visibility = "hidden";
+  dataThur.style.visibility = "hidden";
+  dataSat.style.visibility = "hidden";
+  dataSun.style.visibility = "hidden";
 });
 barSat.addEventListener("click", function () {
   dataSat.style.visibility = "visible";
+  dataMon.style.visibility = "hidden";
+  dataTue.style.visibility = "hidden";
+  dataWed.style.visibility = "hidden";
+  dataThur.style.visibility = "hidden";
+  dataFri.style.visibility = "hidden";
+  dataSun.style.visibility = "hidden";
 });
 barSun.addEventListener("click", function () {
   dataSun.style.visibility = "visible";
+  dataMon.style.visibility = "hidden";
+  dataTue.style.visibility = "hidden";
+  dataWed.style.visibility = "hidden";
+  dataThur.style.visibility = "hidden";
+  dataFri.style.visibility = "hidden";
+  dataSat.style.visibility = "hidden";
 });
